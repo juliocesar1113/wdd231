0ca498aa80b2eae58bb2f7e9e3b47ae3a7c8ce15
juliocesar1113 <juliospm3728@gmail.com>
2026-05-11 20:13:45 -0600

    Initial commit

---
+++ scripts/course.js
@@ -0,0 +1,89 @@
+const courses = [
+    {
+        subject: "CSE",
+        number: 110,
+        title: "Introduction to Programming",
+        credits: 2,
+        completed: true
+    },
+    {
+        subject: "WDD",
+        number: 130,
+        title: "Web Fundamentals",
+        credits: 2,
+        completed: true
+    },
+    {
+        subject: "CSE",
+        number: 111,
+        title: "Programming with Functions",
+        credits: 2,
+        completed: true
+    },
+    {
+        subject: "CSE",
+        number: 210,
+        title: "Programming with Classes",
+        credits: 2,
+        completed: true
+    },
+    {
+        subject: "WDD",
+        number: 131,
+        title: "Dynamic Web Fundamentals",
+        credits: 2,
+        completed: true
+    },
+    {
+        subject: "WDD",
+        number: 231,
+        title: "Web Frontend Development I",
+        credits: 2,
+        completed: false
+    }
+];
+
+const courseContainer = document.querySelector("#course-list");
+const totalCredits = document.querySelector("#total-credits");
+
+function displayCourses(courseList) {
+    courseContainer.innerHTML = "";
+
+    courseList.forEach(course => {
+        const card = document.createElement("div");
+
+        card.classList.add("course-card");
+
+        if (course.completed) {
+            card.classList.add("completed");
+        }
+
+        card.innerHTML = `
+      <h3>${course.subject} ${course.number}</h3>
+      <p>${course.title}</p>
+      <p>${course.credits} Credits</p>
+    `;
+
+        courseContainer.appendChild(card);
+    });
+
+    const credits = courseList.reduce((total, course) => total + course.credits, 0);
+
+    totalCredits.textContent = `Total Credits: ${credits}`;
+}
+
+displayCourses(courses);
+
+document.querySelector("#all").addEventListener("click", () => {
+    displayCourses(courses);
+});
+
+document.querySelector("#cse").addEventListener("click", () => {
+    const cseCourses = courses.filter(course => course.subject === "CSE");
+    displayCourses(cseCourses);
+});
+
+document.querySelector("#wdd").addEventListener("click", () => {
+    const wddCourses = courses.filter(course => course.subject === "WDD");
+    displayCourses(wddCourses);
+});
